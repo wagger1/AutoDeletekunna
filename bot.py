0a55facceb531608e2ad96bc741e2ed45712f11f
Akhil.A.S <119261400+wagger1@users.noreply.github.com>
2025-06-21 11:18:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,11 +17,10 @@ for usr in environ.get("ADMINS").split():
 START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
 
 
-User = Client(session_name=SESSION,
+User = Client(SESSION,
               api_id=API_ID,
               api_hash=API_HASH,
-              workers=300
-              )
+              workers=300)
 
 
 Bot = Client(session_name="auto-delete",
